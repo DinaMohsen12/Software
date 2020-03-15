1cc02aa8e6099e8c62a9843d0c2df6526d8cf12f
DinaMohsen12 <dina.mouhsen2@gmail.com>
2020-03-15 23:55:42 +0200

    Project folder

---
+++ Project/MVClogin/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVClogin.Models;
+namespace MVClogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login lg)
+        {
+            if (ModelState.IsValid) {
+                using (StudentsEntities se = new StudentsEntities()) {
+             var log = se.students.Where(a=>a.Username.Equals(lg.username) && a.Password.Equals(lg.password)).FirstOrDefault();
+                if (log != null)
+                    {
+                   Session["username"] = log.Username;
+               return RedirectToAction("StudentHome", "Home");
+                    }
+               else {
+                        lg.Err = true;
+                         return RedirectToAction("DoSomething", "Home");
+             
+                    }
+                }
+            }
+            return View();
+        }
+        public ActionResult StudentHome() {
+            return View();
+        }
+        public ActionResult DoSomething()
+        {
+            //Your condition where you want to show your message
+            //Add to the model state, your custom error 
+            ModelState.AddModelError(string.Empty, "Username or Password undefiend");
+        return View("login");
+        }
+        public ActionResult Logout() {
+            Session.Clear();
+
+            return RedirectToAction("login","Home"); }
+
+    }
+}
---
+++ Project/MVClogin/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVClogin.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage ="Enter your name")]
+        public string username { get; set; }
+        [Required(ErrorMessage = "Enter your password")]
+        [StringLength(100,ErrorMessage ="should be at least 6 char",MinimumLength =6)]
+        public string password { get; set; }
+        public bool Err { get; set; }
+    }
+}
